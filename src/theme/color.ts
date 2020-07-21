d89546c9ffb26b6e76824002a21c8bcb73b9ec87
Michael_Lee <hello_zaizi@163.com>
2020-07-21 09:57:32 +0800

    增加更多的主题样式

--- src/theme/color.ts
+++ src/theme/color.ts
@@ -1,21 +1,31 @@
 export enum Black {
   L1 = '#000',
+
   L2 = '#333',
+
   L3 = '#666',
+
 }
 
 export enum White {
   L1 = '#fff',
+
   L2 = '#f6f6f6',
 }
 
 export enum Gray {
   L1 = '#ccc',
+
   L2 = '#e2e2e2',
+
   L3 = '#eee',
+
   L4 = '#a2a2a2',
+
   L5 = '#999',
+
   L6 = '#6e6e6e',
+
   L7 = '#666',
 }
 
@@ -25,5 +35,6 @@ export enum Blue {
 
 export enum State {
   Error = '#ff2020',
+  
   Waring = '#ff8500',
 }
