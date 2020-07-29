f19999d1a1580b6cc7ccb22b868e6c3690794df2
Michael_Lee <hello_zaizi@163.com>
2020-07-29 15:36:57 +0800

    useStyles: remove orginal classname of returned classname

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -28,7 +28,7 @@ export const createStyles = (
       }
 
       if (hasOrignalClassName) {
-        return `${styles[originalClassName]} ${originalClassName}` + (className ? ` ${className}` : '');
+        return styles[originalClassName] + (className ? ` ${className}` : '');
       }
 
       if (className) {
